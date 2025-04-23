f7521c032823bdc0733aabcc94c5981027b24f05
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-04-23 09:51:46 -0300

    pedro

--- scripts/home.js
+++ scripts/home.js
@@ -7,7 +7,7 @@ if (carrinho == null) {
     carrinho = [];
 }
 
-fetch('/scripts/dados.json')
+fetch('../dados.json')
     .then(response => response.json())
     .then(data => {
         produtos = data;
